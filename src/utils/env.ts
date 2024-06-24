ebc9480f0c3f18a6af738a7eee305b4838101a8f
George Tsatsaris <tsatsarisgiorgos@gmail.com>
2024-06-24 22:42:14 +0300

    [feat] dockerize init

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
 export function getEnv(key: string) {
     // eslint-disable-next-line security/detect-object-injection
     return process.env[key] || ''
